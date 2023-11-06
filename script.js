06d1c50a814ec1285b95e0b78928a9d054fb053c
BY4j <brandon.y4ng@gmail.com>
2023-11-06 16:35:07 +0100

    Working Version

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-var banner_video = document.getElementById('video-back-loop');
-var banner_image = document.getElementById('img-banner-background');
-var banner_div = document.getElementById('video-back-loop');
+var banner_video = document.getElementById('video-banner-loop');
+var banner_image = document.getElementById('img-banner');
+var h1= document.getElementById('h1-title');
 
 banner_video.addEventListener('ended', function () {
     $(this).fadeOut('5000', function () {
         banner_image.fadeIn('5000');
+        h1.fadeIn('5000');
     });
-    banner_image.style.display = 'block';
+    banner_image.style.display = 'inline';
+    h1.style.display = 'inline';
 }, false);
 
+$(window).on('mousewheel', function() {
+	$('html, body').stop();
+});
 
+
+
+var scroll_vid = $('#video-banner-loop')[0];
+
+$(window).scroll(function(){
+    var scroll = $(this).scrollTop();
+    scroll > 70 ? scroll_vid.pause() : scroll_vid.play();
+  })
