0ea09d7bb809144f0b65eeeaf2ff449fd1ba3795
BY4j <brandon.y4ng@gmail.com>
2023-12-13 09:11:17 +0100

    Last update

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ var h1 = document.getElementById('h1-title');
 
 banner_video.addEventListener('ended', function () {
 
-/*Sa marche absolument pas ! :D En tout cas pas comme prévu. Tant pis*/
+    /*Sa marche absolument pas ! :D En tout cas pas comme prévu. Tant pis*/
 
-    $(this).fadeOut('5000', function () {
-        banner_image.fadeIn('5000');
-        h1.fadeIn('5000');
-    });
+    // $(this).fadeOut('5000', function () {
+    //     banner_image.fadeIn('5000');
+    //     h1.fadeIn('5000');
+    // });
     banner_image.style.display = 'block';
     h1.style.display = 'inline';
 }, false);
@@ -21,7 +21,8 @@ $(window).on('mousewheel', function () {
 
 
 /*Page offset vertical. Pas eu le temps d'investiguer son utilisation*/
-var y = window.scrollY;
+var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
+console.log(scrollPos);
 
 
 /* Sa marche pas, sa saoul. Donc le code à été freeze jusqu'à nouvel ordre*/
@@ -37,12 +38,13 @@ var y = window.scrollY;
 
 /* Vidéos Fast Paced Combat*/
 
-// var scroll_vid_fast = $('#fast-paced-scroll')[0];
+var scroll_vid_fast = $('#fast-paced-scroll')[0];
 
-// $(window).scroll(function () {
-//     var scroll = $(this).scrollTop();
-//     scroll > 2400 ? scroll_vid_fast.pause() : scroll_vid_fast.play();
-// })
+$(window).scroll(function () {
+    var scroll = $(this).scrollTop();
+    if scroll < 2907 {}
+    scroll > 2907 ? scroll_vid_fast.pause() : scroll_vid_fast.play();
+})   3621
 
 /* Vidéos Boss fight prediction*/
 // var scroll_vid_boss = $('#boss-fight-scroll')[0];
